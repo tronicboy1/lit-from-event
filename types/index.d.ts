2fb9d1d685ec3dca6f6c53726123318e8a830828
Austin John Mayer (井上 オースティン) <mayeraus41@gmail.com>
2023-04-21 13:49:00 +0900

    update tsconfig

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,3 +1,6 @@
 import { LitElement } from "lit";
 import { Observable } from "rxjs";
+/**
+ * Use to generate event observables in a LitElement
+ */
 export declare function fromEventQuery<T = Event>(this: LitElement, selector: string, eventName: string): Observable<T>;

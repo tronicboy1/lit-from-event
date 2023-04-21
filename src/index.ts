d5f6a3cdcb784a2335c372e8b5ef1d9b9f63183d
Austin John Mayer (井上 オースティン) <mayeraus41@gmail.com>
2023-04-21 14:22:17 +0900

    change decorator to ts classic decorator

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,27 @@
 import { LitElement } from "lit";
-import { Observable, from, fromEvent, map, switchMap } from "rxjs";
+import { from, fromEvent, map, switchMap } from "rxjs";
 
 /**
  * Use to generate event observables in a LitElement
+ *
+ * @example
+ * @queryFromEvent("audio#river", "play") play$!: Observable<Event>;
  */
-export function fromEventQuery<T = Event>(this: LitElement, selector: string, eventName: string): Observable<T> {
-  return from(this.updateComplete).pipe(
-    map(() => this.renderRoot.querySelector(selector)),
-    switchMap((element) => {
-      if (!element) {
-        throw ReferenceError("NoElementFound");
-      }
-      return fromEvent<T>(element, eventName);
-    })
-  );
+export function queryFromEvent<T = Event>(selector: string, event: string) {
+  return (proto: Object, name: PropertyKey): any => {
+    const descriptor = {
+      get(this: LitElement) {
+        return from(this.updateComplete).pipe(
+          map(() => this.renderRoot.querySelector(selector)),
+          switchMap((element) => {
+            if (!element) throw ReferenceError("NoElementFound");
+            return fromEvent<T>(element!, event);
+          })
+        );
+      },
+      enumerable: true,
+      configurable: true,
+    };
+    return Object.defineProperty(proto, name, descriptor);
+  };
 }
